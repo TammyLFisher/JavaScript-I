b3024d39577fdd06fa1d7bd6e1cdda13986e5391
TammyLFisher <kochira_heruko1@yahoo.com>
2019-06-26 21:52:45 -0500

    function-conversion is done

--- assignments/objects.js
+++ assignments/objects.js
@@ -19,20 +19,52 @@ const example = {
 
 // Write your intern objects here:
 
+let intern1 = {
+  id: 1,
+  name: "Mitzi",
+  email: "mmelloy0@psu.edu",
+  gender: "F"
+}
+let intern2 = {
+  id: 2,
+  name: "Kennen",
+  email: "kdiben1@tinypic.com",
+  gender: "M"
+}
+let intern3 = {
+  id: 3,
+  name: "Keven",
+  email: "kmummery2@wikimedia.org",
+  gender: "M"
+}
+let intern4 = {
+  id: 4,
+  name: "Gannie",
+  email: "gmartinson3@illinois.edu",
+  gender: "M"
+}
+let intern5 = {
+  id: 5,
+  name: "Antonietta",
+  email: "adaine5@samsung.com",
+  gender: "F"
+}
+
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
+console.log(intern1.name);
 
 // Kennan's ID
-
+console.log(intern2.id);
 // Keven's email
-
+console.log(intern3.email);
 // Gannie's name
-
+console.log(intern4.name);
 // Antonietta's Gender
-
+console.log(intern5.name);
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
